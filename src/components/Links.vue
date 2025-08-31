d18d28cad45f9b72a79b9171cbe7695ddf1e6308
cxl2020MC <andamu2019@163.com>
2025-08-31 11:42:55 +0800

    feat(components): 更新 Links 组件中的图标和链接
    - 将 GitHub 图标从 mdi:github 更改为 fa7-brands:github
    - 将 Blog 图标从 mdi:microsoft-bing 更改为 fa7-solid:blog
    - 新增 QQ 群链接和对应的 fa7-brands:qq 图标

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -5,15 +5,21 @@ import { Icon } from "@iconify/vue";
 <template>
     <div class="links">
         <div class="card link">
-            <Icon icon="mdi:github" />
+            <Icon icon="fa7-brands:github" />
             <a href="https://github.com/cxl2020MC">
                 GitHub
             </a>
         </div>
         <div class="card link">
-            <Icon icon="mdi:microsoft-bing" />
+            <Icon icon="fa7-solid:blog" />
             <a href="https://blog.cxl2020mc.top">
                 Blog
+            </a>
+        </div>
+                <div class="card link">
+            <Icon icon="fa7-brands:qq" />
+            <a href="https://qun.qq.com/universal-share/share?ac=1&authKey=4On4X39JdY%2BeTrERg0smNXlyUEPMe1DvML1fcauM6Bp3S26nUPuM4NF3WQleyDwj&busi_data=eyJncm91cENvZGUiOiI4NjA3Njk3NjQiLCJ0b2tlbiI6IkNZQmdWTWlMaVJjd3UxQWp2akVEd0owMkxnMVBjUldoMUtpNUhZa0thOUZDandYUWgxMXlsbnQreUcxZWxxN0MiLCJ1aW4iOiIxMTc2NTAzOTMwIn0%3D&data=5e386OjhF4Ed1Mclz6OTsbgMvTEYdwbe2l_YNFWFXMEB5VAmnPV5GiUeTDqGCwGVakdd4wz5F631nHZ_a7YIaQ&svctype=4&tempid=h5_group_info">
+                QQ群
             </a>
         </div>
     </div>
